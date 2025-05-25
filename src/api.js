dc3a5d789327f933fd406b356400ca96270d99e5
Divya Jayaraman <divyajayaram1210@gmail.com>
2025-05-25 16:51:17 +0530

    Update: latest changes to frontend/backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://stockserver-eight.vercel.app/api',
+  baseURL: 'https://stockserver-eight.vercel.app/',
 });
 
 export default API;
